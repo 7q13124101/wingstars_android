c28126462b3c9bbb9f2116e250aa20ecffd158a4
西安_李浩 <hao_li@newsoft.com.cn>
2025-11-19 13:14:07 +0800

    绘制氛圍時尚->應援服 列表页面

--- member/src/main/java/com/wingstars/member/fragment/SupportSuitFragment.kt
+++ member/src/main/java/com/wingstars/member/fragment/SupportSuitFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wingstars.base.base.BaseFragment
+import com.wingstars.base.utils.DPUtils
+import com.wingstars.base.utils.ScreenUtils
 import com.wingstars.member.adapter.CategoryAdapter
+import com.wingstars.member.adapter.SupportFashionAdapter
+import com.wingstars.member.adapter.SupportSuitAdapter
 import com.wingstars.member.databinding.FragmentSupportSuitBinding
 import com.wingstars.member.viewmodel.RankExplanationViewModel
 import com.wingstars.member.viewmodel.SupportSuitViewModel
@@ -28,6 +33,14 @@ class SupportSuitFragment : BaseFragment() {
     }
 
     private fun initView() {
+        binding.list.layoutManager = GridLayoutManager(requireActivity(),2)
+        val list = mutableListOf("1","2","3","4","5","6")
+        var width = ScreenUtils.getWidth(requireActivity())
+        var smallwidth  = width - DPUtils.dpToPx(50f,requireActivity()).toInt()
+        var smallwidths = smallwidth/2
+        var smallhight = smallwidths.toInt()*1.585
+        binding.list.adapter = SupportSuitAdapter(requireActivity(), list,smallwidths.toInt()
+        ,smallhight.toInt())
         viewModel = ViewModelProvider(this)[SupportSuitViewModel::class.java]
         viewModel.categorylist.observe(viewLifecycleOwner){ list->
             binding.categoryList.layoutManager = LinearLayoutManager(requireActivity(),
