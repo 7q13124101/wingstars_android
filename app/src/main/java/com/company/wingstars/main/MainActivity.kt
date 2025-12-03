3f99cca600345362432fb1443a6dcd3858d40289
西安_李浩 <hao_li@newsoft.com.cn>
2025-12-03 09:20:41 +0800

    固定成员首页标题,实现图框拍照预览

--- app/src/main/java/com/company/wingstars/main/MainActivity.kt
+++ app/src/main/java/com/company/wingstars/main/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.company.wingstars.main
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.company.wingstars.R
 import com.company.wingstars.databinding.ActivityMainBinding
 import com.wingstars.base.base.BaseActivity
+import com.wingstars.base.inter.IPermissionsCallback
 import com.wingstars.calendar.fragment.CalendarFragment
 import com.wingstars.count.fragment.CountFragment
 import com.wingstars.home.fragment.HomeFragment
+import com.wingstars.member.activity.FanInteractionActivity
 import com.wingstars.member.fragment.MemberFragment
 import com.wingstars.user.UserFragment
 
-class MainActivity : BaseActivity(), BaseActivity.OnInitialization, View.OnClickListener {
+class MainActivity : BaseActivity(), BaseActivity.OnInitialization, View.OnClickListener,
+    IPermissionsCallback {
     private lateinit var binding: ActivityMainBinding
     private lateinit var fragments: ArrayList<Fragment>
     private var position = 0
+    private var permission=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -103,6 +111,54 @@ class MainActivity : BaseActivity(), BaseActivity.OnInitialization, View.OnClick
             3-> binding.user.setChecked(false)
         }
         changeTab(4)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+
+        if (requestCode == 1000) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //  runOnUiThread { startActivity(Intent(this@MainActivity, FanInteractionActivity::class.java)) }
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+                    Log.e("permissions","onRequestPermissionsResult permissions=$permissions")
+                    if (permission==1){
+                        runOnUiThread { startActivity(Intent(this@MainActivity, FanInteractionActivity::class.java)) }
+                    }
+
+                } else {
+                    applyPermission1()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+
+    }
+
+    private fun  applyPermission1(){
+        var  permissions = ArrayList<String>();
+        permissions.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this,permissions.toTypedArray(), 1001)
+        } else {
+            if (permission==1){
+                runOnUiThread { startActivity(Intent(this@MainActivity, FanInteractionActivity::class.java)) }
+            }
+
+        }
+    }
+
+
+    override fun setPermissions(permissions: Int) {
+        permission = permissions
+        Log.e("setPermissions","setPermissions=$permissions")
     }
 
 
--- member/src/main/java/com/wingstars/member/fragment/MemberFragment.kt
+++ member/src/main/java/com/wingstars/member/fragment/MemberFragment.kt
@@ -1,6 +1,9 @@
 package com.wingstars.member.fragment
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -8,10 +11,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowInsets
-import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.widget.NestedScrollView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wingstars.base.base.BaseFragment
+import com.wingstars.base.utils.DPUtils
+import com.wingstars.base.utils.ScreenUtils
 import com.wingstars.member.activity.AtmosphereFashionDetailsActivity
 import com.wingstars.member.activity.EventHighlightsActivity
 import com.wingstars.member.activity.ExclusiveSongsListActivity
@@ -19,17 +26,23 @@ import com.wingstars.member.activity.FanInteractionActivity
 import com.wingstars.member.activity.FashionableAtmosphereActivity
 import com.wingstars.member.activity.MemberIntroductionActivity
 import com.wingstars.member.activity.PopularityRankingActivity
-import com.wingstars.member.viewmodel.MemberViewModel
-import com.wingstars.member.adapter.PopularityAdapter
 import com.wingstars.member.adapter.GirlIntroductionAdapter
+import com.wingstars.member.adapter.PopularityAdapter
 import com.wingstars.member.adapter.SupportFashionAdapter
 import com.wingstars.member.databinding.FragmentMemberBinding
+import com.wingstars.member.viewmodel.MemberViewModel
+import com.wingstars.base.inter.IPermissionsCallback
+import com.wingstars.base.net.NetBase
+
 
 class MemberFragment : BaseFragment(), View.OnClickListener,
     PopularityAdapter.onPopularityRankingListener, SupportFashionAdapter.onSupportFashionListener {
     private lateinit var viewModel: MemberViewModel
     private lateinit var binding: FragmentMemberBinding
-    private var statusBarHeight = 0
+    private var maxHight  = 0
+    private var currentHeight = 0
+    private var minHight=0
+    private lateinit var permissionsCallback:IPermissionsCallback
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,16 +56,74 @@ class MemberFragment : BaseFragment(), View.OnClickListener,
 
 
     private fun initView() {
+        /*binding.image.post {
+            maxHight = binding.image.height
+            currentHeight = maxHight
+        }*/
+
+        val account = NetBase.decrypt(NetBase.WINGSTARS_ACCOUNT_ENC)
+        val password = NetBase.decrypt(NetBase.WINGSTARS_PASSWORD_ENC)
+
+        val authorization =
+            NetBase.base64Encode("$account:$password".toByteArray(Charsets.UTF_8))
+        Log.e("authorization","authorization=$authorization")
+
+        val params = binding.image!!.layoutParams
+        var smallwidth = ScreenUtils.getWidth(requireActivity())
+        maxHight = (smallwidth*0.965).toInt()
+        currentHeight = maxHight
+        params.width = smallwidth
+        params.height = maxHight
+        binding.image!!.setLayoutParams(params)
+        binding.scroll.setOnScrollChangeListener(object: NestedScrollView.OnScrollChangeListener{
+            override fun onScrollChange(
+                v: NestedScrollView,
+                scrollX: Int,
+                scrollY: Int,
+                oldScrollX: Int,
+                oldScrollY: Int
+            ) {
+                val isScrollingDown = scrollY > oldScrollY
+                val isScrollingUp = scrollY < oldScrollY
+               // Log.e("scrollY","scrollY=$scrollY oldScrollY=$oldScrollY")
+                if (isScrollingDown){
+                    val i = Math.abs(scrollY - oldScrollY)
+                    if (currentHeight>minHight){
+                        currentHeight = if (currentHeight-i>minHight)  currentHeight-i else minHight
+                        val params = binding.image!!.layoutParams
+                        params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                        params.height = currentHeight
+                        binding.image!!.setLayoutParams(params)
+                    }
+                }
+                if (isScrollingUp){
+                    if (scrollY<=maxHight-minHight){
+                        val i = Math.abs(scrollY - oldScrollY)
+                        if (currentHeight<maxHight){
+                            currentHeight = if (currentHeight+i>maxHight)  maxHight else currentHeight+i
+                            val params = binding.image!!.layoutParams
+                            params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                            params.height = currentHeight
+                            binding.image!!.setLayoutParams(params)
+                        }
+                    }
+
+                }
+            }
+
+        })
         viewModel = ViewModelProvider(this)[MemberViewModel::class.java]
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
             binding.root.setOnApplyWindowInsetsListener { v, insets ->
                 val statusBarHeight = insets.getInsets(WindowInsets.Type.statusBars()).top
+                minHight = statusBarHeight+ DPUtils.dpToPx(64f,requireActivity()).toInt()
                 Log.e("statusBarHeight", "statusBarHeight=$statusBarHeight")
                 setViewTop(binding.title, statusBarHeight)
                 binding.root.setOnApplyWindowInsetsListener(null)
                 insets
             }
         } else {
+            minHight = getStatusBarHeight()+ DPUtils.dpToPx(64f,requireActivity()).toInt()
             setViewTop(binding.title, getStatusBarHeight())
         }
 
@@ -90,7 +161,7 @@ class MemberFragment : BaseFragment(), View.OnClickListener,
 
 
     public fun setViewTop(view: View, top: Int) {
-        val layoutParams = view.layoutParams as LinearLayout.LayoutParams
+        val layoutParams = view.layoutParams as RelativeLayout.LayoutParams
         layoutParams.topMargin = top
         view.setLayoutParams(layoutParams);
     }
@@ -106,12 +177,12 @@ class MemberFragment : BaseFragment(), View.OnClickListener,
                 )
             )
 
-            binding.take.id -> startActivity(
+            binding.take.id -> applyPermission()/*startActivity(
                 Intent(
                     requireActivity(),
                     FanInteractionActivity::class.java
                 )
-            )
+            )*/
 
             binding.llEventHighlights.id -> startActivity(
                 Intent(
@@ -141,6 +212,53 @@ class MemberFragment : BaseFragment(), View.OnClickListener,
                 )
             )
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is IPermissionsCallback) {
+            permissionsCallback = context
+        } else {
+            throw RuntimeException("$context must implement IActivityCallback")
+        }
+    }
+    private fun applyPermission() {
+        permissionsCallback.setPermissions(1)
+        var permission = ArrayList<String>();
+        permission.add(Manifest.permission.CAMERA)
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(requireActivity(), permission.toTypedArray(), 1000)
+        } else {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+                jumpTakePhoto()
+            } else {
+                applyPermission1()
+            }
+
+        }
+    }
+
+    private fun applyPermission1() {
+        var permission = ArrayList<String>();
+        permission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(requireActivity(), permission.toTypedArray(), 1001)
+        } else {
+            jumpTakePhoto()
+        }
+    }
+
+    private fun jumpTakePhoto() {
+        startActivity(Intent(requireActivity(), FanInteractionActivity::class.java))
     }
 
     override fun onPopularityRankingClickItem(position: Int) {
